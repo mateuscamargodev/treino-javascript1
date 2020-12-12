be41039ec7e466a6bfcece2b2abdecde45b77871
Mateus <mateuscamargo929@gmail.com>
2020-12-12 18:44:44 -0300

    Fazendo ajustes no index.html e começando a criar a página de login do administrador

---
+++ trata_dados.js
@@ -0,0 +1,11 @@
+var botaoCadastro = document.querySelector('#btn-cadastro');
+
+botaoCadastro.addEventListener('click', function(){
+    var nomeProduto = document.querySelector('#nome-produto').value;
+    var preçoProduto = document.querySelector('#preco-produto').value;
+
+    document.cookie = ;
+
+    console.log(document.cookie);
+})
+
